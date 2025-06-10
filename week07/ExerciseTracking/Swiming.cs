d8072e92a3a9280b7b5b344f8034d70e231b09ec
ahiamatah <ahiamatah@gmail.com>
2025-06-10 05:17:27 +0000

    Added week 7 assignment

---
+++ week07/ExerciseTracking/Swiming.cs
@@ -0,0 +1,16 @@
+class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int lengthInMinutes, int laps)
+        : base(date, lengthInMinutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance() => _laps * 50 / 1000.0 * 0.62;
+
+    public override double GetSpeed() => (GetDistance() / GetLengthInMinutes()) * 60;
+
+    public override double GetPace() => GetLengthInMinutes() / GetDistance();
+}
